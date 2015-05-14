b7ca65f334280422c86091032aa054922395d213
ScienceARK <djkleeven@hotmail.com>
2015-05-14 04:11:26 +0200

    Version

--- MagicalGirlLux/Properties/AssemblyInfo.cs
+++ MagicalGirlLux/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Magical Girl Lux")]
+[assembly: AssemblyTitle("MagicalGirlLux")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Magical Girl Lux")]
+[assembly: AssemblyProduct("MagicalGirlLux")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
